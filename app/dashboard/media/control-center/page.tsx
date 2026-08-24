9234d1d4d1f3aaeff5df60e1d87bb1f5d464a4f2
vivitgroup <groupvivit@gmail.com>
2026-08-24 17:38:02 +0300

    Enable live media sync on control center

--- app/dashboard/media/control-center/page.tsx
+++ app/dashboard/media/control-center/page.tsx
@@ -1,3 +1,4 @@
 import {MediaControlV2} from "@/components/media/MediaControlV2";
+import {MediaLiveSync} from "@/components/media/MediaLiveSync";
 export const dynamic="force-dynamic";
-export default function MediaControlCenter(){return <MediaControlV2/>}
+export default function MediaControlCenter(){return <><MediaLiveSync/><MediaControlV2/></>}

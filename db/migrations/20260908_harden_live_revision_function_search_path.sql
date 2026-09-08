2b5425f130ea61443edbef471e333c61cc7cff9a
vivitgroup <groupvivit@gmail.com>
2026-09-08 15:15:20 +0300

    security(realtime): harden revision trigger functions

---
+++ db/migrations/20260908_harden_live_revision_function_search_path.sql
@@ -0,0 +1,2 @@
+alter function public.bump_live_workspace_revision() set search_path = pg_catalog, public;
+alter function public.bump_notification_live_revision() set search_path = pg_catalog, public;
